587498df79844ab00a8b0527d06c06b44313c701
Alkxyly <alkxyly@hotmail.com>
2017-12-11 20:31:26 -0200

    ContratoBean and page xhtml/jsf

--- src/main/java/br/com/sergipetec/controller/ContratoBean.java
+++ src/main/java/br/com/sergipetec/controller/ContratoBean.java
@@ -1,8 +1,31 @@
 package br.com.sergipetec.controller;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
-@ManagedBean
+import br.com.sergipetec.dao.ContratoRN;
+import br.com.sergipetec.model.Contrato;
+
+@ManagedBean(name = "contratoBean")
+@RequestScoped
 public class ContratoBean {
+	private Contrato contrato;
+	
+	public ContratoBean(){
+		this.contrato =  new Contrato();
+	}
+	
+	public void salvar(){
+		ContratoRN contratoRN = new ContratoRN();
+	}
 
+	public Contrato getContrato() {
+		return contrato;
+	}
+
+	public void setContrato(Contrato contrato) {
+		this.contrato = contrato;
+	}
+	
+	
 }
--- src/main/java/br/com/sergipetec/dao/ContratoRN.java
+++ src/main/java/br/com/sergipetec/dao/ContratoRN.java
@@ -1,5 +1,18 @@
 package br.com.sergipetec.dao;
 
-public class ContratoRN {
+import br.com.sergipetec.model.Contrato;
+import br.com.sergipetec.web.filter.DAOFactory;
 
+public class ContratoRN {
+	
+	private ContratoDAO contratoDAO;
+	
+	public ContratoRN(){
+		this.contratoDAO = DAOFactory.criarContratoDAO();
+	}
+	
+	public void salvar(Contrato contrato){
+		if(contrato == null || contrato.getId() == 0)
+			this.contratoDAO.salvar(contrato);
+	}
 }
